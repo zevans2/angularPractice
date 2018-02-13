5dbf79d0060a54eeb809eac7f0771e1d338778ea
Zach <zevans2@missouriwestern.edu>
2018-02-13 00:01:14 -0600

    Initial Commit. Created Components for structure of application. Created Authentication service and connected to firebase.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCUpUgG7hSFvezYlypnR7x7HtklXD9CpcI',
+    authDomain: 'testshop-2ce1a.firebaseapp.com',
+    databaseURL: 'https://testshop-2ce1a.firebaseio.com',
+    projectId: 'testshop-2ce1a',
+    storageBucket: '',
+    messagingSenderId: '69042928059'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCUpUgG7hSFvezYlypnR7x7HtklXD9CpcI',
+    authDomain: 'testshop-2ce1a.firebaseapp.com',
+    databaseURL: 'https://testshop-2ce1a.firebaseio.com',
+    projectId: 'testshop-2ce1a',
+    storageBucket: '',
+    messagingSenderId: '69042928059'
+  }
 };
